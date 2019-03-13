9c573ea5128211fda8f49e587e4b78d107f3f77e
Thomas Zwaanswijk <s1089273@student.windesheim.nl>
2019-03-13 16:00:39 +0100

    Arduino can now send the data in a format that can be read byte for byte by the Pi Started documentation to ensure future groups can work on this.

--- code/MPUbasic/MPUbasic/MPUbasic.ino
+++ code/MPUbasic/MPUbasic/MPUbasic.ino
@@ -28,19 +28,18 @@
 #include "MPU9250.h"
 
 #define AHRS false         // Set to false for basic data read
-#define SerialDebug true  // Set to true to get Serial output for debugging
+const int kQuaternionMultFact = 100;
 
 // Pin definitions
 int intPin = 12;  // These can be changed, 2 and 3 are the Arduinos ext int pins
 int myLed  = 13;  // Set up pin 13 led for toggling
 
+uint8_t message_count = 0;
+
 #define I2Cclock 400000
 #define I2Cport Wire
 #define MPU9250_ADDRESS MPU9250_ADDRESS_AD0   // Use either this line or the next to select which I2C address your device is using
 //#define MPU9250_ADDRESS MPU9250_ADDRESS_AD1
-
-uint8_t buffer_to_pi[50]; //change size to fit amount of data later for optimization.
-
 
 MPU9250 myIMU(MPU9250_ADDRESS, I2Cport, I2Cclock);
 
@@ -64,8 +63,6 @@ void setup()
 
   if (c == 0x73) // WHO_AM_I should always be 0x71, but the chip used could be a knockoff - TODO document issues.
   {
-    //Serial.println(F("MPU9250 is online..."));
-
     // Start by performing self test
     myIMU.MPU9250SelfTest(myIMU.selfTest);
 
@@ -96,11 +93,7 @@ void setup()
     myIMU.getAres();
     myIMU.getGres();
     myIMU.getMres();
-
-    // The next call delays for 4 seconds, and then records about 15 seconds of
-    // data to calculate bias and scale.
-    myIMU.magCalMPU9250(myIMU.magBias, myIMU.magScale);
-    delay(2000); // Add delay to see results before serial spew of data
+    
   } // if (c == 0x73)
   else
   {
@@ -116,39 +109,46 @@ void setup()
 
 void loop()
 {
+  int16_t accel_data[3];
+  int16_t gyro_data[3];
+  int16_t mag_data[3];
+  for(uint8_t i = 0; i < 3; i++) {
+    accel_data[i] = myIMU.accelCount[i];
+    gyro_data[i] = myIMU.gyroCount[i];
+    mag_data[i] = myIMU.magCount[i];
+  }
   // If intPin goes high, all data registers have new data
   // On interrupt, check if data ready interrupt
   if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
   {
     
     myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
-/*
-    // Now we'll calculate the accleration value into actual g's
-    // This depends on scale being set
-    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - myIMU.accelBias[0];
-    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - myIMU.accelBias[1];
-    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - myIMU.accelBias[2];
-*/
     
     myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
-/*
+
+    myIMU.readMagData(myIMU.magCount);  // Read the x/y/z adc values
+    
+    // Now we'll calculate the accleration value into actual g's
+    // This depends on scale being set
+    myIMU.ax = (float)accel_data[0] * myIMU.aRes; // - myIMU.accelBias[0];
+    myIMU.ay = (float)accel_data[1] * myIMU.aRes; // - myIMU.accelBias[1];
+    myIMU.az = (float)accel_data[2] * myIMU.aRes; // - myIMU.accelBias[2];
+    
     // Calculate the gyro value into actual degrees per second
     // This depends on scale being set
-    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
-    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
-    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
-*/
-    myIMU.readMagData(myIMU.magCount);  // Read the x/y/z adc values
-
+    myIMU.gx = (float)gyro_data[0] * myIMU.gRes;
+    myIMU.gy = (float)gyro_data[1] * myIMU.gRes;
+    myIMU.gz = (float)gyro_data[2] * myIMU.gRes;
+    
     // Calculate the magnetometer values in milliGauss
     // Include factory calibration per data sheet and user environmental
     // corrections
     // Get actual magnetometer value, this depends on scale being set
-    myIMU.mx = (float)myIMU.magCount[0] * myIMU.mRes
+    myIMU.mx = (float)mag_data[0] * myIMU.mRes
                * myIMU.factoryMagCalibration[0] - myIMU.magBias[0];
-    myIMU.my = (float)myIMU.magCount[1] * myIMU.mRes
+    myIMU.my = (float)mag_data[1] * myIMU.mRes
                * myIMU.factoryMagCalibration[1] - myIMU.magBias[1];
-    myIMU.mz = (float)myIMU.magCount[2] * myIMU.mRes
+    myIMU.mz = (float)mag_data[2] * myIMU.mRes
                * myIMU.factoryMagCalibration[2] - myIMU.magBias[2];
   } // if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
 
@@ -163,6 +163,8 @@ void loop()
   // along the x-axis just like in the LSM9DS0 sensor. This rotation can be
   // modified to allow any convenient orientation convention. This is ok by
   // aircraft orientation standards! Pass gyro rate as rad/s
+  //madgwick is more intensive, but more accurate. If the arduino cannot deal with the amount of calculations
+  //switch to Mahony.
   MadgwickQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx * DEG_TO_RAD,
                          myIMU.gy * DEG_TO_RAD, myIMU.gz * DEG_TO_RAD, myIMU.my,
                          myIMU.mx, myIMU.mz, myIMU.deltat);
@@ -172,74 +174,55 @@ void loop()
 
   myIMU.count = millis();
   digitalWrite(myLed, !digitalRead(myLed));  // toggle led - kept in to make it easier to check whether or not the arduino has crashed.
-
-// print startbytes
-    Serial.print('$');
-    Serial.print(0x03, HEX);
-// print quaternion values, order  = Qw, Qx, Qy, Qz - important to have this match on the receiving end!
-  for(int i = 0;i<=3;i++) {
-    Serial.print(*getQ() + i);
+  
+  Serial.print('$');
+  Serial.print(0x03);
+  // transmit quaternion bytes
+  // rather than transmit floats, which take a lot of bytes (4 each) instead they are multiplied by 100 (ignoring evrything more than 2 decimal places behind the 0
+  // This allows for them to be transmitted as integers, without significant impact.
+  // Kind of a workaround, there is probably a neater method out there.
+  for(uint8_t i = 0; i < 4; i++) {
+    int8_t quaternion_value = (*(getQ() + i)) * kQuaternionMultFact;
+    Serial.print(quaternion_value);
   }
-// print accelerometer values
-    Serial.print(myIMU.ax);
-    Serial.print(myIMU.ay);
-    Serial.print(myIMU.az);
-// print gyro values
-    Serial.print(myIMU.gx);
-    Serial.print(myIMU.gy);
-    Serial.print(myIMU.gz);
-//print magnetometer values
-    Serial.print(myIMU.mx);
-    Serial.print(myIMU.my);
-    Serial.print(myIMU.mz);
-//print temperature after turning it into degrees celcius.
-    myIMU.temperature = ((float) myIMU.tempCount) / 333.87 + 21.0;
-    Serial.print(myIMU.temperature);
-
-
-
-
-  //Roll, Yaw and Pitch are not used by ROS, but have been left in in case it could be useful to include in the project at a future date
-  //============================================================================
-  // Define output variables from updated quaternion---these are Tait-Bryan
-  // angles, commonly used in aircraft orientation. In this coordinate system,
-  // the positive z-axis is down toward Earth. Yaw is the angle between Sensor
-  // x-axis and Earth magnetic North (or true North if corrected for local
-  // declination, looking down on the sensor positive yaw is counterclockwise.
-  // Pitch is angle between sensor x-axis and Earth ground plane, toward the
-  // Earth is positive, up toward the sky is negative. Roll is angle between
-  // sensor y-axis and Earth ground plane, y-axis up is positive roll. These
-  // arise from the definition of the homogeneous rotation matrix constructed
-  // from quaternions. Tait-Bryan angles as well as Euler angles are
-  // non-commutative; that is, the get the correct orientation the rotations
-  // must be applied in the correct order which for this configuration is yaw,
-  // pitch, and then roll.
-  // For more see
-  // http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-  // which has additional links.
-  //============================================================================
-//  myIMU.yaw   = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ()
-//                              * *(getQ() + 3)), *getQ() * *getQ() + * (getQ() + 1)
-//                      * *(getQ() + 1) - * (getQ() + 2) * *(getQ() + 2) - * (getQ() + 3)
-//                      * *(getQ() + 3));
-//  myIMU.pitch = -asin(2.0f * (*(getQ() + 1) * *(getQ() + 3) - *getQ()
-//                              * *(getQ() + 2)));
-//  myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ() + 1) + * (getQ() + 2)
-//                              * *(getQ() + 3)), *getQ() * *getQ() - * (getQ() + 1)
-//                      * *(getQ() + 1) - * (getQ() + 2) * *(getQ() + 2) + * (getQ() + 3)
-//                      * *(getQ() + 3));
-//  myIMU.pitch *= RAD_TO_DEG;
-//  myIMU.yaw   *= RAD_TO_DEG;
-//============================================================================
-
-  // again, not used in the project ATM, but if it is going to be used, remember to check the value once per year and update it, or find some way to have WTR do it automatically
-  // - http://www.ngdc.noaa.gov/geomag-web/#declination
-  // - check the longitude and latitude for windesheim - should be 2.45 +- 0.37 for every year since 2019-03-19
-  //myIMU.yaw  -= 2.45;
-  //myIMU.roll *= RAD_TO_DEG;
-//============================================================================
+  // transmit accelerometer data
+  /*
+   * The data here is split into two bytes.
+   * This is done because serial communication was previously done from a FIFO buffer which was just copied into another buffer
+   * In order to minimize the amount of work needed on the receiving end, this format is expanded with the magnetometer values.
+   * The Pi expects a high and low byte for every bit of data, so that is what it will receive.
+   * All of the sensor readings are in reality 16 bit integers, split into two 8-bit integers for the sake of transmission
+   * The bytes are labeled "xxx_high" and "xxx_low" for the sake of clarity.
+   * these are then transmitted separately.
+   */
+  for(uint8_t i = 0; i < 3; i++) {
+    uint8_t accel_high = ( accel_data[i] >> 8);     
+    uint8_t accel_low = (0xff & accel_data[i]);
+    Serial.print(accel_high);
+    Serial.print(accel_low);
+  }
+  // transmit gyroscope data
+  for(uint8_t i = 0; i < 3; i++) {
+    uint8_t gyro_high = ( gyro_data[i] >> 8);
+    uint8_t gyro_low = (0xff & gyro_data[i]);
+    Serial.print(gyro_high);
+    Serial.print(gyro_low);
+  }
+  // transmit magnetometer data
+  for(uint8_t i = 0; i < 3; i++) {
+    uint8_t mag_high = ( mag_data[i] >> 8);
+    uint8_t mag_low = (0xff & mag_data[i]);
+    Serial.print(mag_high);
+    Serial.print(mag_low);
+  }
+  Serial.print(message_count);
+  Serial.print("\r\n"); // terminating characters for the raspberry Pi
+  if(message_count == 255) { //message count loops on 255
+    message_count = 0;
+  } else {
+    message_count++;
+  }
   myIMU.count = millis();
   myIMU.sumCount = 0;
   myIMU.sum = 0;
-  //delay(500);
 }
