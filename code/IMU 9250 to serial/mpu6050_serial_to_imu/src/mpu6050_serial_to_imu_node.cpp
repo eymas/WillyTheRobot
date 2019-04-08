3f23d0ac53ddcf7b05eb22b3fa69d25083585d0f
Thomas Zwaanswijk <s1089273@student.windesheim.nl>
2019-04-08 11:19:29 +0200

    Removed superfluous casts, since a string is already an array of chars, so selecting a single value from that string is automatically a char.

--- code/IMU 9250 to serial/mpu6050_serial_to_imu/src/mpu6050_serial_to_imu_node.cpp
+++ code/IMU 9250 to serial/mpu6050_serial_to_imu/src/mpu6050_serial_to_imu_node.cpp
@@ -138,19 +138,19 @@ int main(int argc, char** argv)
                 tf::Quaternion differential_rotation;
                 differential_rotation = zero_orientation.inverse() * orientation;
                 // get accelerometer values
-                int16_t ax = (((char)input[data_packet_start + 6] << 8)  | (char)input[data_packet_start] + 7);
-                int16_t ay = (((char)input[data_packet_start + 8] << 8)  | (char)input[data_packet_start] + 9);
-                int16_t az = (((char)input[data_packet_start + 10] << 8) | (char)input[data_packet_start] + 11);
+                int16_t ax = ((input[data_packet_start + 6] << 8)  | input[data_packet_start] + 7);
+                int16_t ay = ((input[data_packet_start + 8] << 8)  | input[data_packet_start] + 9);
+                int16_t az = ((input[data_packet_start + 10] << 8) | input[data_packet_start] + 11);
 
-                // get gyro values
-                int16_t gx = (((char)input[data_packet_start + 12] << 8) | (char)input[data_packet_start] + 13);
-                int16_t gy = (((char)input[data_packet_start + 14] << 8) | (char)input[data_packet_start] + 15);
-                int16_t gz = (((char)input[data_packet_start + 16] << 8) | (char)input[data_packet_start] + 17);
+                // get gyro valuess
+                int16_t gx = ((input[data_packet_start + 12] << 8) | input[data_packet_start] + 13);
+                int16_t gy = ((input[data_packet_start + 14] << 8) | input[data_packet_start] + 15);
+                int16_t gz = ((input[data_packet_start + 16] << 8) | input[data_packet_start] + 17);
 
                 // get magnetometer values
-                int16_t mx = (((char)input[data_packet_start + 18] << 8) | (char)input[data_packet_start] + 19);
-                int16_t my = (((char)input[data_packet_start + 20] << 8) | (char)input[data_packet_start] + 21);
-                int16_t mz = (((char)input[data_packet_start + 22] << 8) | (char)input[data_packet_start] + 23);
+                int16_t mx = ((input[data_packet_start + 18] << 8) | input[data_packet_start] + 19);
+                int16_t my = ((input[data_packet_start + 20] << 8) | input[data_packet_start] + 21);
+                int16_t mz = ((input[data_packet_start + 22] << 8) | input[data_packet_start] + 23);
 
                 // calculate rotational velocities in rad/s
                 // without the last factor the velocities were too small
