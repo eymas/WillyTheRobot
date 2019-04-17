01587a2308172c01327cabe63669bc120d997c4a
Thomas Zwaanswijk <s1089273@student.windesheim.nl>
2019-04-17 09:30:14 +0200

    added brackets to hopefully fix the issue of invalid operands.

--- code/IMU 9250 to serial/mpu6050_serial_to_imu/src/mpu6050_serial_to_imu_node.cpp
+++ code/IMU 9250 to serial/mpu6050_serial_to_imu/src/mpu6050_serial_to_imu_node.cpp
@@ -148,19 +148,19 @@ int main(int argc, char** argv)
                 float ax, az, ay, gx, gy, gz, mx, my, mz;
                 for(uint8_t i = 0; i < 4; i++) {
                     // get accelerometer values
-                     ax = (ax << 8 | static_cast<uint8_t>(input[data_packet_start+5+i]));
-                     ay = (ay << 8 | static_cast<uint8_t>(input[data_packet_start+9+i]));
-                     az = (az << 8 | static_cast<uint8_t>(input[data_packet_start+13+i]));
+                     ax = ((ax << 8) | static_cast<uint8_t>(input[data_packet_start+5+i]));
+                     ay = ((ay << 8) | static_cast<uint8_t>(input[data_packet_start+9+i]));
+                     az = ((az << 8) | static_cast<uint8_t>(input[data_packet_start+13+i]));
                     std::cout << "AX: " << ax << " AY: " << ay << " AZ: " << az << '\n';
                     // get gyro values
-                     gx = (gx << 8 | static_cast<uint8_t>(input[data_packet_start+17+i]));
-                     gy = (gy << 8 | static_cast<uint8_t>(input[data_packet_start+21+i]));
-                     gz = (gz << 8 | static_cast<uint8_t>(input[data_packet_start+25+i]));
+                     gx = ((gx << 8) | static_cast<uint8_t>(input[data_packet_start+17+i]));
+                     gy = ((gy << 8) | static_cast<uint8_t>(input[data_packet_start+21+i]));
+                     gz = ((gz << 8) | static_cast<uint8_t>(input[data_packet_start+25+i]));
                     std::cout << "GX: " << gx << " GY: " << gy << " GZ: " << gz << '\n';
                     // get magnetometer values
-                     mx = (mx << 8 | static_cast<uint8_t>(input[data_packet_start+29+i]));
-                     my = (my << 8 | static_cast<uint8_t>(input[data_packet_start+33+i]));
-                     mz = (mz << 8 | static_cast<uint8_t>(input[data_packet_start+37+i]));
+                     mx = ((mx << 8) | static_cast<uint8_t>(input[data_packet_start+29+i]));
+                     my = ((my << 8) | static_cast<uint8_t>(input[data_packet_start+33+i]));
+                     mz = ((mz << 8) | static_cast<uint8_t>(input[data_packet_start+37+i]));
                     std::cout << "MX: " << mx << " MY: " << my << " MZ: " << mz << '\n';
                 }
                 // calculate rotational velocities in rad/s
