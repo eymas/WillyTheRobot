e2a92ad34dc8783b71de57a1c51df3af5891a927
Tom van den Noort <tvdn24@gmail.com>
2019-05-15 13:19:52 +0200

    Updated quaternion orientations, added reference to them in NESW directions

--- code/demo_script/demo_move_to_goal.py
+++ code/demo_script/demo_move_to_goal.py
@@ -7,6 +7,13 @@ from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 
 #Pose(Point(x, y, z), Quaternion(x, y, z, w)),
 #Pose(Point(1.0, 0.0, 0.0), Quaternion(0.0, 0.0, 0.99, 1.0)),
+
+# Quaternion orientations (rounded):
+# North: 0 0 0 1
+# East: 0 0 -0.7 0.7
+# South: 0 0 1 0
+# West: 0 0 0.7 0.7
+
 positions = []
 
 # Callback that gets run once amcl_pose publishes the current position of the robot
@@ -16,18 +23,19 @@ def pose_callback(msg):
     pose = msg.position
     orientation = msg.orientation
     print("-------------- current position --------------")
-    print(msg.pose.pose)
+    print(pose)
+    print(orientation)
     # Form new pose.
     new_pose = Pose(Point(pose.x + 1.0, pose.y, pose.z), Quaternion(orientation.x, orientation.y, orientation.z, orientation.w))
     print("-------------- new position     --------------")
     print(new_pose)
     # Keep on forging new poses until the cube's complete.
     npp1 = new_pose
-    new_pose2 = Pose(Point(npp1.position.x, npp1.position.y + 1.0, npp1.position.z), Quaternion(npp1.orientation.x, npp1.orientation.y, npp1.orientation.z + 0.5, npp1.orientation.w))
+    new_pose2 = Pose(Point(npp1.position.x, npp1.position.y + 1.0, npp1.position.z), Quaternion(npp1.orientation.x, npp1.orientation.y, npp1.orientation.z - 0.7, npp1.orientation.w - 0.3))
     npp2 = new_pose2
-    new_pose3 = Pose(Point(npp2.position.x - 1.0, npp2.position.y, npp2.position.z), Quaternion(npp2.orientation.x, npp2.orientation.y, npp2.orientation.z + 0.5, npp2.orientation.w))
+    new_pose3 = Pose(Point(npp2.position.x - 1.0, npp2.position.y, npp2.position.z), Quaternion(npp2.orientation.x, npp2.orientation.y, npp2.orientation.z + 1.7, npp2.orientation.w - 0.7))
     npp3 = new_pose3
-    new_pose4 = Pose(Point(npp3.position.x, npp3.position.y - 1.0, npp3.position.z), Quaternion(npp3.orientation.x, npp3.orientation.y, npp3.orientation.z + 0.5, npp3.orientation.w))
+    new_pose4 = Pose(Point(npp3.position.x, npp3.position.y - 1.0, npp3.position.z), Quaternion(npp3.orientation.x, npp3.orientation.y, npp3.orientation.z - 0.3, npp3.orientation.w + 0.7))
     # Then throw them in an array for the movebase_client to process
     positions.extend([new_pose, new_pose2, new_pose3, new_pose4])
 
